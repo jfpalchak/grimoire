ca19124f1b307908f4aa35556fdb84c48a4ee088
Joey Palchak <joey.palchak@gmail.com>
2024-04-25 18:47:56 -0700

    add util to calculate ability score modifier

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -21,10 +21,22 @@ export function formatMD(text: string[]): string {
 // return the string with emphasis (in markdown),
 // otherwise return the string without emphasis.
 function highlight(row: string): string {
-  const hasSavingThrow = row.match('make.*saving throw|must.*saving throw.*$');
+  const hasSavingThrow = row.match('(make.*saving throw|must.*saving throw).*$');
   const isNotFormatted = !(row.startsWith('-') || row.startsWith('*'));
   if (hasSavingThrow && isNotFormatted) {
-    return row = '_' + row + '_';
+    return '_' + row + '_';
   }
   return row;
+}
+
+// Calculate the modifier of a given ability score.
+// Possible modifiers range from -5 to +10.
+export function modifier(score: number): number {
+  if (score < 1) {
+    return -5;
+  } else if (score > 30) {
+    return 10;
+  } else {
+    return Math.floor((score - 10) / 2);
+  }
 }
