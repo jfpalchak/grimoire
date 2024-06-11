d7acd736e76b5c426d789cbcfee864f18b546763
Joey Palchak <joey.palchak@gmail.com>
2024-06-11 12:29:58 -0700

    update rest types & err handling

--- src/lib/rest/fetch.ts
+++ src/lib/rest/fetch.ts
@@ -1,24 +1,20 @@
 import { API_URL } from "@/config";
 
-export const fetchData = async (query: string) => {
-  try {
-    const res = await fetch(`${API_URL}${query}`, {
-      method: 'GET',
-      headers: {
-        'Accept': 'application/json',
-      },
-    });
+export const fetchData = async <T>(query: string): Promise<T> => {
+  const res = await fetch(`${API_URL}${query}`, {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+    },
+  });
 
-    if (!res.ok) {
-      throw new Error(`Network response not OK. Status: ${res.status} ${res.statusText}`);
-    }
-
-    const data = await res.json();
-    
-    return data;
-  } catch (error) {
-    console.log(`Error fetching '${query}':`, error);
+  if (!res.ok) {
+    throw new Error(`Network response not OK. Status: ${res.status} ${res.statusText}`);
   }
+
+  const data = await res.json();
+  
+  return data;
 };
 
 export const dndRest = {
--- src/lib/rest/services.ts
+++ src/lib/rest/services.ts
@@ -10,27 +10,43 @@ import type {
   RulesSubsection,
 } from "@/types";
 
-export const getSpell = async (index: string): Promise<Spell> => {
-  return await dndRest.get(`spells/${index}`);
+export const getSpell = async (index: string): Promise<Spell | undefined> => {
+  try {
+    return await dndRest.get<Spell>(`spells/${index}`);
+  } catch (err) {
+    console.log('Problem fetching spell: ', err);
+  }
 };
 
-export const getMonster = async (index: string): Promise<Monster> => {
-  return await dndRest.get(`monsters/${index}`);
+export const getMonster = async (index: string): Promise<Monster | undefined> => {
+  try {
+    return await dndRest.get<Monster>(`monsters/${index}`);
+  } catch (err) {
+    console.log('Problem fetching monster: ', err);
+  }
 };
 
-export const getEquipment = async (index: string): Promise<Equipment> => {
-  return await dndRest.get(`equipment/${index}`);
+export const getEquipment = async (index: string): Promise<Equipment | undefined> => {
+  try {
+    return await dndRest.get<Equipment>(`equipment/${index}`);
+  } catch (err) {
+    console.log('Problem fetching equipment: ', err);
+  }
 };
 
-export const getMagicItem = async (index: string): Promise<MagicItem> => {
-  return await dndRest.get(`magic-items/${index}`);
+export const getMagicItem = async (index: string): Promise<MagicItem | undefined> => {
+  try {
+    return await dndRest.get<MagicItem>(`magic-items/${index}`);
+  } catch (err) {
+    console.log('Problem fetching magic item: ', err);
+  }
 }
 
 export const getRules = async (index: string): Promise<RulesChapter | undefined> => {
   try {
-    const rules: Rules = await dndRest.get(`rules/${index}`);
+    const rules = await dndRest.get<Rules>(`rules/${index}`);
 
-    const sectionPromises: Promise<RulesSubsection>[] = rules.subsections.map(({ url }) => dndRest.get(shortUrl(url)));
+    const sectionPromises = rules.subsections.map(({ url }) => dndRest.get<RulesSubsection>(shortUrl(url)));
     const subsections = await Promise.all(sectionPromises);
 
     return { rules, subsections };
